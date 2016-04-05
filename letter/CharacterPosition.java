ab884e574f297addb784ac931e64880b48598f83
Kyra Tijhuis <kyratijhuis@outlook.com>
2016-04-05 09:12:42 +0200

    Laatste commit van desktop PC

--- letter/CharacterPosition.java
+++ letter/CharacterPosition.java
@@ -4,13 +4,12 @@ public class CharacterPosition {
 	
 
 	public static void main(String... args) {
-		CharacterPosition obj = new CharacterPosition();
 		if (args.length==1 && args[0].length()==1) {
 			String character = args[0].toLowerCase();
 			char ch = character.charAt(0);
 			if (Character.isLetter(ch)) {
 				int position = ch - 'a';
-				System.out.println(position);
+				System.out.println(position + 1);
 			} else {
 				System.out.println("Please enter a letter");
 			}
